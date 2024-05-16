cae99e37541bd1b7c85197923ff26488b02af01e
Alex Dewar <a.dewar@imperial.ac.uk>
2024-05-16 11:10:03 +0100

    Move various variables out of solve()

--- src/muse2.rs
+++ src/muse2.rs
@@ -1,31 +1,38 @@
-use good_lp::{highs, variables, Solution, SolverModel};
+use good_lp::{
+    highs, solvers::highs::HighsSolution, variables, Constraint, Expression, ProblemVariables,
+    Solution, SolverModel,
+};
 
-fn solve() -> Option<(f64, f64)> {
-    // Create variables in a readable format with a macro...
+fn solve(
+    vars: ProblemVariables,
+    objective: Expression,
+    constraints: Vec<Constraint>,
+) -> Option<HighsSolution> {
+    let mut model = vars.maximise(objective).using(highs);
+    for constraint in constraints.into_iter() {
+        model.add_constraint(constraint);
+    }
+
+    model.solve().ok()
+}
+
+fn solve_toy_problem() -> (f64, f64) {
     variables! {
     vars:
         a <= 1;
         2 <= b <= 4;
     }
 
-    // ... or add variables programmatically
-    // vars.add(variable().min(2).max(9));
+    let objective = 10 * (a - b / 5) - b;
+    let constraints = vec![(a + 2.) << b, (1 + a) >> (4. - b)];
 
-    let solution = vars
-        .maximise(10 * (a - b / 5) - b)
-        .using(highs)
-        .with((a + 2.) << b) // or (a + 2).leq(b)
-        .with((1 + a) >> (4. - b))
-        .solve()
-        .ok()?;
+    let solution = solve(vars, objective, constraints).unwrap();
 
-    Some((solution.value(a), solution.value(b)))
+    (solution.value(a), solution.value(b))
 }
 
 pub fn run() {
-    println!("Hello from MUSE 2.0!");
-
-    let (a, b) = solve().expect("Failed to compute solution");
+    let (a, b) = solve_toy_problem();
 
     println!("Calculated solution: a = {}, b = {}", a, b);
 }
@@ -37,7 +44,7 @@ mod tests {
 
     #[test]
     fn test_solve() {
-        let (a, b) = solve().unwrap();
+        let (a, b) = solve_toy_problem();
         assert_f64_near!(a, 1.);
         assert_f64_near!(b, 3.);
     }
