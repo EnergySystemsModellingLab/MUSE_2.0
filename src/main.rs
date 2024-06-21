bd13b12631db4f274996f8e03802586222aabb13
Ashmit8583 <137117727+Ashmit8583@users.noreply.github.com>
2024-06-21 15:45:57 +0530

    Update main.rs
    Modified it according to the regions.rs file
    
    Closes #58

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 mod regions;
-
-use std::path::Path;
-use regions::read_regions_from_csv;
+mod simulation;
 
 fn main() {
-    let file_path = Path::new("regions.csv");
-    
-    match read_regions_from_csv(file_path) {
-        Ok(regions) => {
-            for region in regions {
-                println!("Short Name: {}, Description: {}", region.short_name, region.description);
-            }
-        }
-        Err(err) => {
-            eprintln!("Error reading regions from CSV: {}", err);
-        }
-    }
+    // Initialize the simulation
+    simulation::initialize_simulation();
 }
