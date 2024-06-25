43c36f02ed6d7e0c1d868d94e2d0520bbd329b25
Ashmit8583 <137117727+Ashmit8583@users.noreply.github.com>
2024-06-25 14:17:43 +0530

    Update main.rs
    Updated the main.rs file to the previous one

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-mod settings;
+//! Provides the main entry point to the program.
+
 mod simulation;
 
+use std::env;
+use std::path::Path;
+
+/// The main entry point to the program
 fn main() {
-    // Call the simulation initialization
-    simulation::initialize_simulation();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Must provide path to model configuration TOML file.");
+    }
+
+    simulation::run(Path::new(&args[1]))
 }
