856a81b0152fa9f80fd92e228b3d3849bf36182e
Alex Dewar <a.dewar@imperial.ac.uk>
2024-07-02 16:19:36 +0100

    Initialise logger on program start

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,20 @@ use std::path::Path;
 
 /// The main entry point to the program
 fn main() {
+    // Log at the info level by default, unless the user has specified otherwise
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info")
+    }
+
+    // Initialise logger
+    env_logger::init();
+
+    // Parse command-line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         panic!("Must provide path to model configuration TOML file.");
     }
 
+    // Run simulation
     simulation::run(Path::new(&args[1]))
 }
