79faa662977b9b0671f40343170dd378b7b0cac2
abhinavgupta2004 <abhinavgupta.iitd@gmail.com>
2024-07-12 17:45:48 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ mod simulation;
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        panic!("Usage: muse2 <config_file_path>");
+        panic!("Usage: muse2 <config_file_path> <csv_file_path>");
     }
 
     let config_file_path = Path::new(&args[1]);
+    // Assuming the second argument is now the CSV file path
+    let csv_file_path = Path::new(&args[2]);
 
-    simulation::run(config_file_path)
+    simulation::run(config_file_path, csv_file_path)
 }
