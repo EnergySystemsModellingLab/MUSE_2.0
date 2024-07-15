0ce143a2fa38e2bd6dbf7187b205f70c8256b3a5
Ashmit <ashmit.sikdar2004@gmail.com>
2024-07-15 15:34:45 +0530

    a

--- src/settings.rs
+++ src/settings.rs
@@ -10,6 +10,8 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 /// Model settings
+
+#[allow(dead_code)]
 pub struct Settings {
     pub time_slices: Vec<TimeSlice>,
     pub milestone_years: Vec<u32>,
