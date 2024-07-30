3a78ec97e95524d17415fb63847ce309d0f29b15
Alex Dewar <a.dewar@imperial.ac.uk>
2024-07-30 17:42:12 +0100

    Read in agent_objectives.csv
    Closes #107.

--- src/agent.rs
+++ src/agent.rs
@@ -2,12 +2,15 @@ use crate::input::*;
 use crate::region::*;
 use serde::Deserialize;
 use serde_string_enum::DeserializeLabeledStringEnum;
+
 use std::collections::{HashMap, HashSet};
+use std::error::Error;
 use std::path::Path;
 use std::rc::Rc;
 
 const AGENT_FILE_NAME: &str = "agents.csv";
 const AGENT_REGIONS_FILE_NAME: &str = "agent_regions.csv";
+const AGENT_OBJECTIVES_FILE_NAME: &str = "agent_objectives.csv";
 
 #[derive(Debug, PartialEq)]
 pub enum SearchSpace {
@@ -55,6 +58,8 @@ pub struct Agent {
 
     #[serde(skip)]
     pub regions: RegionSelection,
+    #[serde(skip)]
+    pub objectives: Vec<AgentObjective>,
 }
 
 macro_rules! define_agent_id_getter {
@@ -74,6 +79,99 @@ struct AgentRegion {
 }
 define_agent_id_getter! {AgentRegion}
 define_region_id_getter! {AgentRegion}
+
+/// **TODO** Add more objective types
+#[derive(Debug, PartialEq, DeserializeLabeledStringEnum)]
+pub enum ObjectiveType {
+    #[string = "lcox"]
+    LevellisedCostOfX,
+    #[string = "eac"]
+    EquivalentAnnualCost,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub struct AgentObjective {
+    agent_id: String,
+    objective_type: ObjectiveType,
+    decision_weight: Option<f64>,
+    decision_lexico_tolerance: Option<f64>,
+}
+define_agent_id_getter! {AgentObjective}
+
+/// Check that required parameters are present and others are absent
+fn check_objective_parameter(objective: &AgentObjective, agent: &Agent) -> Result<(), String> {
+    // Check that the user hasn't supplied a value for a field we're not using
+    macro_rules! check_field_none {
+        ($field:ident) => {
+            if objective.$field.is_some() {
+                Err(format!(
+                    "Field {} should be empty for this decision rule",
+                    stringify!($field)
+                ))?;
+            }
+        };
+    }
+
+    // Check that required fields are present
+    macro_rules! check_field_some {
+        ($field:ident) => {
+            if objective.$field.is_none() {
+                Err(format!("Required field {} is empty", stringify!($field)))?;
+            }
+        };
+    }
+
+    match &agent.decision_rule {
+        DecisionRule::Single => {
+            check_field_none!(decision_weight);
+            check_field_none!(decision_lexico_tolerance);
+        }
+        DecisionRule::Weighted => {
+            check_field_none!(decision_lexico_tolerance);
+            check_field_some!(decision_weight);
+        }
+        DecisionRule::Lexicographical => {
+            check_field_none!(decision_weight);
+            check_field_some!(decision_lexico_tolerance);
+        }
+    };
+
+    Ok(())
+}
+
+fn read_agent_objectives_from_iter<I>(
+    iter: I,
+    agents: &HashMap<Rc<str>, Agent>,
+    agent_ids: &HashSet<Rc<str>>,
+) -> Result<HashMap<Rc<str>, Vec<AgentObjective>>, Box<dyn Error>>
+where
+    I: Iterator<Item = AgentObjective>,
+{
+    let objectives = iter.into_id_map(agent_ids)?;
+    for objective in objectives.values().flatten() {
+        // We've already checked that agent IDs are valid
+        let agent = agents.get(objective.agent_id.as_str()).unwrap();
+
+        // Check that required parameters are present and others are absent
+        check_objective_parameter(objective, agent)?;
+    }
+
+    if objectives.len() < agent_ids.len() {
+        Err("All agents must have at least one objective")?;
+    }
+
+    Ok(objectives)
+}
+
+fn read_agent_objectives(
+    model_dir: &Path,
+    agents: &HashMap<Rc<str>, Agent>,
+    agent_ids: &HashSet<Rc<str>>,
+) -> HashMap<Rc<str>, Vec<AgentObjective>> {
+    let file_path = model_dir.join(AGENT_OBJECTIVES_FILE_NAME);
+    read_agent_objectives_from_iter(read_csv(&file_path), agents, agent_ids)
+        .unwrap_input_err(&file_path)
+}
 
 /// Read agents info from a CSV file.
 pub fn read_agents_file(
@@ -113,9 +211,11 @@ pub fn read_agents(
     let file_path = model_dir.join(AGENT_REGIONS_FILE_NAME);
     let mut agent_regions =
         read_regions_for_entity::<AgentRegion>(&file_path, &agent_ids, region_ids);
+    let mut objectives = read_agent_objectives(model_dir, &agents, &agent_ids);
 
     for (id, agent) in agents.iter_mut() {
         agent.regions = agent_regions.remove(id).unwrap();
+        agent.objectives = objectives.remove(id).unwrap();
     }
 
     agents
