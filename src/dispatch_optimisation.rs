91216eec0b2e3145882d19ba6084ed455deed556
Alex Dewar <a.dewar@imperial.ac.uk>
2024-08-05 19:01:31 +0100

    Add time elements to example

--- src/dispatch_optimisation.rs
+++ src/dispatch_optimisation.rs
@@ -4,17 +4,29 @@ use log::info;
 pub fn run_dispatch_optimisation(model: &Model) {
     info!("Starting dispatch optimisation");
 
-    for region_id in model.regions.keys() {
-        info!("Looking at region {region_id}");
+    for year in model.iter_years() {
+        info!("Year: {year}");
+        for time_slice in model.iter_time_slices() {
+            info!("Time slice: {time_slice}");
 
-        for asset in model.assets_by_region.get(region_id).unwrap().iter() {
-            info!(
-                "Asset: process {}, agent {}, capacity {}, commission year {}",
-                asset.process.id, asset.agent_id, asset.capacity, asset.commission_year
-            );
+            for region_id in model.regions.keys() {
+                info!("Looking at region {region_id}");
 
-            let pac = asset.process.pacs.first().unwrap();
-            info!("First PAC: {} ({})", pac.id, pac.description);
+                for asset in model.assets_by_region.get(region_id).unwrap().iter() {
+                    if asset.commission_year < year {
+                        // Asset hasn't been commissioned yet
+                        continue;
+                    }
+
+                    info!(
+                        "Asset: process {}, agent {}, capacity {}, commission year {}",
+                        asset.process.id, asset.agent_id, asset.capacity, asset.commission_year
+                    );
+
+                    let pac = asset.process.pacs.first().unwrap();
+                    info!("First PAC: {} ({})", pac.id, pac.description);
+                }
+            }
         }
     }
 }
