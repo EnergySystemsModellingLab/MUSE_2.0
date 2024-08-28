fa25e6b7239200b9ed539309d447de918b5a8d30
Alex Dewar <a.dewar@imperial.ac.uk>
2024-08-28 16:32:22 +0100

    Add some missing doc comments

--- src/agent.rs
+++ src/agent.rs
@@ -11,6 +11,7 @@ const AGENT_FILE_NAME: &str = "agents.csv";
 const AGENT_REGIONS_FILE_NAME: &str = "agent_regions.csv";
 const AGENT_OBJECTIVES_FILE_NAME: &str = "agent_objectives.csv";
 
+/// Which processes apply to this agent
 #[derive(Debug, Clone, PartialEq)]
 pub enum SearchSpace {
     AllProcesses,
@@ -33,6 +34,7 @@ impl<'de> Deserialize<'de> for SearchSpace {
     }
 }
 
+/// The decision rule for a particular objective
 #[derive(Debug, Clone, PartialEq, DeserializeLabeledStringEnum)]
 pub enum DecisionRule {
     #[string = "single"]
@@ -43,6 +45,7 @@ pub enum DecisionRule {
     Lexicographical,
 }
 
+/// An agent in the simulation
 #[derive(Debug, Deserialize, PartialEq, Clone)]
 pub struct Agent {
     pub id: Rc<str>,
@@ -80,6 +83,8 @@ struct AgentRegion {
 define_agent_id_getter! {AgentRegion}
 define_region_id_getter! {AgentRegion}
 
+/// The type of objective for the agent
+///
 /// **TODO** Add more objective types
 #[derive(Debug, Clone, PartialEq, DeserializeLabeledStringEnum)]
 pub enum ObjectiveType {
@@ -89,6 +94,7 @@ pub enum ObjectiveType {
     EquivalentAnnualCost,
 }
 
+/// An objective for an agent with associated parameters
 #[derive(Debug, Clone, Deserialize, PartialEq)]
 pub struct AgentObjective {
     agent_id: String,
@@ -172,6 +178,15 @@ where
     Ok(objectives)
 }
 
+/// Read agent objective info from the agent_objectives.csv file.
+///
+/// # Arguments
+///
+/// * `model_dir` - Folder containing model configuration files
+///
+/// # Returns
+///
+/// A map of Agents, with the agent ID as the key
 fn read_agent_objectives(
     model_dir: &Path,
     agents: &HashMap<Rc<str>, Agent>,
@@ -207,7 +222,16 @@ where
     Ok(agents)
 }
 
-/// Read agents info from a CSV file.
+/// Read agents info from the agents.csv file.
+///
+/// # Arguments
+///
+/// * `model_dir` - Folder containing model configuration files
+/// * `process_ids` - The possible valid process IDs
+///
+/// # Returns
+///
+/// A map of Agents, with the agent ID as the key
 pub fn read_agents_file(
     model_dir: &Path,
     process_ids: &HashSet<Rc<str>>,
@@ -216,7 +240,17 @@ pub fn read_agents_file(
     read_agents_file_from_iter(read_csv(&file_path), process_ids).unwrap_input_err(&file_path)
 }
 
-/// Read agents info from CSV files.
+/// Read agents info from various CSV files.
+///
+/// # Arguments
+///
+/// * `model_dir` - Folder containing model configuration files
+/// * `process_ids` - The possible valid process IDs
+/// * `region_ids` - The possible valid region IDs
+///
+/// # Returns
+///
+/// A map of Agents, with the agent ID as the key
 pub fn read_agents(
     model_dir: &Path,
     process_ids: &HashSet<Rc<str>>,
