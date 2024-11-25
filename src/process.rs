21b91a583fac3828dcd42897f47eb99f570f984a
Alex Dewar <a.dewar@imperial.ac.uk>
2024-11-25 08:38:30 +0000

    Make bespoke `DemandMap` type with `get` method

--- src/process.rs
+++ src/process.rs
@@ -439,6 +439,7 @@ pub fn read_processes(
 #[cfg(test)]
 mod tests {
     use crate::commodity::{CommodityCostMap, CommodityType};
+    use crate::demand::DemandMap;
     use crate::time_slice::TimeSliceLevel;
 
     use super::*;
@@ -734,7 +735,7 @@ mod tests {
                     kind: CommodityType::InputCommodity,
                     time_slice_level: TimeSliceLevel::Annual,
                     costs: CommodityCostMap::new(),
-                    demand_by_region: HashMap::new(),
+                    demand: DemandMap::new(),
                 };
 
                 (Rc::clone(&commodity.id), commodity.into())
