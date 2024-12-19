5f7bcf3ecf0258bea6b01514222990d099d3bb4a
Ryan Smith <ryan.smith@imperial.ac.uk>
2024-12-19 18:20:28 +0000

    Refactor and move processing logic into read_process_flows_from_iter. Fix tests.

--- src/process.rs
+++ src/process.rs
@@ -299,29 +299,28 @@ fn read_process_availabilities(
 
 fn read_process_flows_from_iter<I>(
     iter: I,
+    process_ids: &HashSet<Rc<str>>,
     commodities: &HashMap<Rc<str>, Rc<Commodity>>,
-) -> Result<Vec<ProcessFlow>>
+) -> Result<HashMap<Rc<str>, Vec<ProcessFlow>>>
 where
     I: Iterator<Item = ProcessFlowRaw>,
 {
-    let mut flows = Vec::new();
-    for flow_raw in iter {
+    iter.map(|flow_raw| -> Result<ProcessFlow> {
         let commodity = commodities
             .get(flow_raw.commodity_id.as_str())
             .with_context(|| format!("{} is not a valid commodity ID", &flow_raw.commodity_id))?;
 
-        let flow = ProcessFlow {
+        Ok(ProcessFlow {
             process_id: flow_raw.process_id,
             commodity: Rc::clone(commodity),
             flow: flow_raw.flow,
             flow_type: flow_raw.flow_type,
             flow_cost: flow_raw.flow_cost,
-        };
-
-        flows.push(flow);
-    }
-
-    Ok(flows)
+        })
+    })
+    .collect::<Result<Vec<_>>>()?
+    .into_iter()
+    .into_id_map(process_ids)
 }
 
 fn read_process_flows(
@@ -330,18 +329,9 @@ fn read_process_flows(
     commodities: &HashMap<Rc<str>, Rc<Commodity>>,
 ) -> Result<HashMap<Rc<str>, Vec<ProcessFlow>>> {
     let file_path = model_dir.join(PROCESS_FLOWS_FILE_NAME);
-    let mut process_flow_raws: HashMap<Rc<str>, Vec<ProcessFlowRaw>> =
-        read_csv_grouped_by_id(&file_path, process_ids)?;
-
-    let mut process_flows = HashMap::new();
-
-    for process_id in process_ids.iter() {
-        let iter = process_flow_raws.remove(process_id).unwrap().into_iter();
-        let flows = read_process_flows_from_iter(iter, commodities)?;
-        process_flows.insert(process_id.to_owned(), flows);
-    }
-
-    Ok(process_flows)
+    let process_flow_csv = read_csv(&file_path)?;
+    read_process_flows_from_iter(process_flow_csv, process_ids, commodities)
+        .with_context(|| input_err_msg(&file_path))
 }
 
 fn read_process_parameters_from_iter<I>(
@@ -495,6 +485,7 @@ pub fn read_processes(
 
 #[cfg(test)]
 mod tests {
+
     use crate::commodity::{CommodityCostMap, CommodityType};
     use crate::time_slice::TimeSliceLevel;
 
@@ -641,6 +632,7 @@ mod tests {
 
     #[test]
     fn test_read_process_flows_from_iter_good() {
+        let process_ids = ["id1".into(), "id2".into()].into_iter().collect();
         let commodities: HashMap<Rc<str>, Rc<Commodity>> = ["commodity1", "commodity2"]
             .into_iter()
             .map(|id| {
@@ -681,36 +673,47 @@ mod tests {
             },
         ];
 
-        let expected: Vec<ProcessFlow> = vec![
-            ProcessFlow {
-                process_id: "id1".into(),
-                commodity: commodities.get("commodity1").unwrap().clone(),
-                flow: 1.0,
-                flow_type: FlowType::Fixed,
-                flow_cost: 1.0,
-            },
-            ProcessFlow {
-                process_id: "id1".into(),
-                commodity: commodities.get("commodity2").unwrap().clone(),
-                flow: 1.0,
-                flow_type: FlowType::Fixed,
-                flow_cost: 1.0,
-            },
-            ProcessFlow {
-                process_id: "id2".into(),
-                commodity: commodities.get("commodity1").unwrap().clone(),
-                flow: 1.0,
-                flow_type: FlowType::Fixed,
-                flow_cost: 1.0,
-            },
-        ];
+        let expected = HashMap::from([
+            (
+                "id1".into(),
+                vec![
+                    ProcessFlow {
+                        process_id: "id1".into(),
+                        commodity: commodities.get("commodity1").unwrap().clone(),
+                        flow: 1.0,
+                        flow_type: FlowType::Fixed,
+                        flow_cost: 1.0,
+                    },
+                    ProcessFlow {
+                        process_id: "id1".into(),
+                        commodity: commodities.get("commodity2").unwrap().clone(),
+                        flow: 1.0,
+                        flow_type: FlowType::Fixed,
+                        flow_cost: 1.0,
+                    },
+                ],
+            ),
+            (
+                "id2".into(),
+                vec![ProcessFlow {
+                    process_id: "id2".into(),
+                    commodity: commodities.get("commodity1").unwrap().clone(),
+                    flow: 1.0,
+                    flow_type: FlowType::Fixed,
+                    flow_cost: 1.0,
+                }],
+            ),
+        ]);
 
-        let actual = read_process_flows_from_iter(flows_raw.into_iter(), &commodities).unwrap();
+        let actual =
+            read_process_flows_from_iter(flows_raw.into_iter(), &process_ids, &commodities)
+                .unwrap();
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn test_read_process_flows_from_iter_bad_commodity_id() {
+        let process_ids = ["id1".into(), "id2".into()].into_iter().collect();
         let commodities = ["commodity1", "commodity2"]
             .into_iter()
             .map(|id| {
@@ -744,7 +747,10 @@ mod tests {
             },
         ];
 
-        assert!(read_process_flows_from_iter(flows_raw.into_iter(), &commodities).is_err());
+        assert!(
+            read_process_flows_from_iter(flows_raw.into_iter(), &process_ids, &commodities)
+                .is_err()
+        );
     }
 
     #[test]
