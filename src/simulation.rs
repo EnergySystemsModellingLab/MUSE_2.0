a3232d1242444d97ae0e10cb391bed5ec814765c
Alex Dewar <a.dewar@imperial.ac.uk>
2025-01-09 16:54:17 +0000

    Print cost coeffs in simulation loop

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,6 +1,59 @@
 //! Functionality for running the MUSE 2.0 simulation.
+use std::rc::Rc;
+
 use crate::model::Model;
-use log::info;
+use crate::process::{Process, ProcessFlow};
+use crate::time_slice::TimeSliceID;
+use itertools::Itertools;
+use log::*;
+
+fn calculate_cost_coeff(
+    year: u32,
+    region_id: &Rc<str>,
+    process: &Process,
+    flow: &ProcessFlow,
+    time_slice: &TimeSliceID,
+) -> f64 {
+    // NB: Whether this cost applies or not will depend on what kind of flow this is, so this is
+    // also wrong
+    let flow_cost = flow.flow_cost;
+
+    let commodity = &flow.commodity;
+
+    // Only applies if commodity is PAC
+    let var_opex = if process
+        .pacs
+        .iter()
+        .map(|pac| &pac.id)
+        .contains(&commodity.id)
+    {
+        info!("It's a PAC");
+        process.parameter.variable_operating_cost
+    } else {
+        info!("It's NOT a PAC");
+        0.0
+    };
+
+    // NB: Need to check balance type
+    let commodity_cost = match flow
+        .commodity
+        .costs
+        .get(region_id.clone(), year, time_slice.clone())
+    {
+        None => {
+            warn!("Commodity cost not found");
+            0.0
+        }
+        Some(cost) => {
+            info!("Commodity cost found :-D");
+            cost.value
+        }
+    };
+
+    // Calculation from dispatch optimisation formulation (with the caveat that each of these values
+    // is probably wrong :-))
+    var_opex + flow_cost + commodity_cost
+}
 
 /// Run the simulation.
 ///
@@ -20,6 +73,12 @@ pub fn run(model: &Model) {
 
                 for flow in asset.process.flows.iter() {
                     info!("**** Commodity: {}", flow.commodity.id);
+
+                    // Just calculate for one time slice for now
+                    let time_slice = model.time_slice_info.iter().next().unwrap();
+                    let coeff =
+                        calculate_cost_coeff(year, region_id, &asset.process, flow, time_slice);
+                    info!("**** Coefficient: {coeff}");
                 }
             }
         }
