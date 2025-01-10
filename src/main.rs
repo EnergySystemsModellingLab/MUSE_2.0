de252929179144b673dfe7c0f655d2b98569206b
sahil raja <s.raja@imperial.ac.uk>
2025-01-10 11:40:50 +0000

    adding back the comments

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
 mod tests {
     use super::*;
     use std::path::{Path, PathBuf};
-
+    /// Get the path to the example model.
     fn get_model_dir() -> PathBuf {
         Path::new(file!())
             .parent()
@@ -30,10 +30,12 @@ mod tests {
             .join("examples")
             .join("simple")
     }
-
+    /// An integration test for the `run` command.
     #[test]
     fn test_handle_run_command() {
         handle_run_command(&get_model_dir()).unwrap();
+
+        // Second time will fail because the logging is already initialised
         assert_eq!(
             handle_run_command(&get_model_dir())
                 .unwrap_err()
