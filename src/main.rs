f8fc444f0ce99f60d615da87b067044937887c48
sahil raja <s.raja@imperial.ac.uk>
2025-01-10 12:09:01 +0000

    moving tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
-
-mod commands;
+use muse2::commands;
 
 use commands::{
     handle_example_list_command, handle_run_command, Cli, Commands, ExampleSubcommands,
@@ -15,35 +14,4 @@ fn main() {
         },
     }
     .unwrap_or_else(|err| eprintln!("{:?}", err))
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::path::{Path, PathBuf};
-    /// Get the path to the example model.
-    fn get_model_dir() -> PathBuf {
-        Path::new(file!())
-            .parent()
-            .unwrap()
-            .parent()
-            .unwrap()
-            .join("examples")
-            .join("simple")
-    }
-    /// An integration test for the `run` command.
-    #[test]
-    fn test_handle_run_command() {
-        handle_run_command(&get_model_dir()).unwrap();
-
-        // Second time will fail because the logging is already initialised
-        assert_eq!(
-            handle_run_command(&get_model_dir())
-                .unwrap_err()
-                .chain()
-                .next()
-                .unwrap()
-                .to_string(),
-            "Failed to initialize logging."
-        );
-    }
 }
