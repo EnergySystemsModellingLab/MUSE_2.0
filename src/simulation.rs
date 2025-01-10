943a59cc0ebb64ddefefe1896ba6a9c5d7f646a3
Alex Dewar <a.dewar@imperial.ac.uk>
2025-01-10 12:43:13 +0000

    Add basic optimisation

--- src/simulation.rs
+++ src/simulation.rs
@@ -4,8 +4,78 @@ use std::rc::Rc;
 use crate::model::Model;
 use crate::process::{Process, ProcessFlow};
 use crate::time_slice::TimeSliceID;
+use highs::{HighsModelStatus, RowProblem};
 use itertools::Itertools;
 use log::*;
+
+/// Run the simulation.
+///
+/// # Arguments:
+///
+/// * `model` - The model to run
+pub fn run(model: &Model) {
+    for year in model.iter_years() {
+        info!("* Milestone year: {year}");
+        perform_dispatch(model, year);
+    }
+}
+
+fn perform_dispatch(model: &Model, year: u32) {
+    let mut problem = RowProblem::default();
+
+    let _ = add_variables(&mut problem, model, year);
+
+    // // Add constraints
+    // for constraint in constraints.iter() {
+    //     if constraint.coefficients.len() != vars.len() {
+    //         panic!("Wrong number of variables specified for constraint");
+    //     }
+
+    //     let mut coeffs = Vec::with_capacity(vars.len());
+    //     for (var, coeff) in vars.iter().zip(constraint.coefficients.iter()) {
+    //         coeffs.push((*var, *coeff));
+    //     }
+
+    //     problem.add_row(constraint.min..=constraint.max, coeffs);
+    // }
+
+    let solved = problem.optimise(highs::Sense::Minimise).solve();
+    let status = solved.status();
+    if status != HighsModelStatus::Optimal {
+        panic!("Could not solve: {status:?}");
+    }
+
+    let solution = solved.get_solution();
+    info!("Solution: {:?}", solution.columns());
+}
+
+fn add_variables(problem: &mut RowProblem, model: &Model, year: u32) -> Vec<highs::Col> {
+    let mut vars = Vec::new();
+    for region_id in model.iter_regions() {
+        info!("** Region: {region_id}");
+        for asset in model.get_assets(year, region_id) {
+            info!(
+                "*** Agent {} has asset {} (commissioned in {})",
+                asset.agent_id, asset.process.id, asset.commission_year
+            );
+
+            for flow in asset.process.flows.iter() {
+                info!("**** Commodity: {}", flow.commodity.id);
+
+                // Just calculate for one time slice for now
+                let time_slice = model.time_slice_info.iter().next().unwrap();
+                let coeff = calculate_cost_coeff(year, region_id, &asset.process, flow, time_slice);
+                info!("**** Coefficient: {coeff}");
+
+                // **HACK**: We need bounds, so just make some up for now
+                let bounds = -100..=100;
+                vars.push(problem.add_column(coeff, bounds));
+            }
+        }
+    }
+
+    vars
+}
 
 fn calculate_cost_coeff(
     year: u32,
@@ -53,34 +123,4 @@ fn calculate_cost_coeff(
     // Calculation from dispatch optimisation formulation (with the caveat that each of these values
     // is probably wrong :-))
     var_opex + flow_cost + commodity_cost
-}
-
-/// Run the simulation.
-///
-/// # Arguments:
-///
-/// * `model` - The model to run
-pub fn run(model: &Model) {
-    for year in model.iter_years() {
-        info!("* Milestone year: {year}");
-        for region_id in model.iter_regions() {
-            info!("** Region: {region_id}");
-            for asset in model.get_assets(year, region_id) {
-                info!(
-                    "*** Agent {} has asset {} (commissioned in {})",
-                    asset.agent_id, asset.process.id, asset.commission_year
-                );
-
-                for flow in asset.process.flows.iter() {
-                    info!("**** Commodity: {}", flow.commodity.id);
-
-                    // Just calculate for one time slice for now
-                    let time_slice = model.time_slice_info.iter().next().unwrap();
-                    let coeff =
-                        calculate_cost_coeff(year, region_id, &asset.process, flow, time_slice);
-                    info!("**** Coefficient: {coeff}");
-                }
-            }
-        }
-    }
 }
