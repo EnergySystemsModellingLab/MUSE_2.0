471ab9be5e5b095dd3097b2f9c0076b4f283809b
Alex Dewar <a.dewar@imperial.ac.uk>
2025-01-24 10:56:02 +0000

    Add tests for reading in process availabilities

--- src/input/process/availability.rs
+++ src/input/process/availability.rs
@@ -81,3 +81,138 @@ where
 
     Ok(map)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::time_slice::TimeSliceID;
+    use itertools::assert_equal;
+    use std::iter;
+
+    fn get_time_slice_info() -> TimeSliceInfo {
+        let slices = [
+            TimeSliceID {
+                season: "winter".into(),
+                time_of_day: "day".into(),
+            },
+            TimeSliceID {
+                season: "summer".into(),
+                time_of_day: "night".into(),
+            },
+        ];
+
+        TimeSliceInfo {
+            seasons: ["winter".into(), "summer".into()].into_iter().collect(),
+            times_of_day: ["day".into(), "night".into()].into_iter().collect(),
+            fractions: [(slices[0].clone(), 0.5), (slices[1].clone(), 0.5)]
+                .into_iter()
+                .collect(),
+        }
+    }
+
+    fn check_succeeds(process_id: &str, limit_type: &str, time_slice: &str, value: f64) -> bool {
+        let process_ids = iter::once("process1".into()).collect();
+        let time_slice_info = get_time_slice_info();
+
+        let avail = ProcessAvailabilityRaw {
+            process_id: process_id.into(),
+            limit_type: limit_type.into(),
+            time_slice: time_slice.into(),
+            value,
+        };
+        read_process_availabilities_from_iter(iter::once(avail), &process_ids, &time_slice_info)
+            .is_ok()
+    }
+
+    #[test]
+    fn test_read_process_availabilities_from_iter_success() {
+        let process_ids = iter::once("process1".into()).collect();
+        let time_slice_info = get_time_slice_info();
+
+        let value = 0.5;
+        macro_rules! check_with_limit_type {
+            ($limit_type: expr, $range: expr) => {
+                let avail = ProcessAvailabilityRaw {
+                    process_id: "process1".into(),
+                    limit_type: $limit_type.into(),
+                    time_slice: "winter".into(),
+                    value,
+                };
+                let time_slice = time_slice_info
+                    .get_time_slice_id_from_str("winter.day")
+                    .unwrap();
+                let expected_map = iter::once((time_slice, $range)).collect();
+                let expected = iter::once(("process1".into(), expected_map));
+                assert_equal(
+                    read_process_availabilities_from_iter(
+                        iter::once(avail),
+                        &process_ids,
+                        &time_slice_info,
+                    )
+                    .unwrap(),
+                    expected,
+                );
+            };
+        }
+
+        check_with_limit_type!("lo", value..=f64::INFINITY);
+        check_with_limit_type!("up", f64::NEG_INFINITY..=value);
+        check_with_limit_type!("fx", value..=value);
+    }
+
+    #[test]
+    fn test_read_process_availabilities_from_iter_values() {
+        macro_rules! succeeds_with_value {
+            ($value:expr) => {{
+                check_succeeds("process1", "fx", "winter.day", $value)
+            }};
+        }
+
+        // Good values
+        assert!(succeeds_with_value!(0.0));
+        assert!(succeeds_with_value!(0.5));
+        assert!(succeeds_with_value!(1.0));
+
+        // Bad values
+        assert!(!succeeds_with_value!(-1.0));
+        assert!(!succeeds_with_value!(2.0));
+        assert!(!succeeds_with_value!(f64::NAN));
+        assert!(!succeeds_with_value!(f64::NEG_INFINITY));
+        assert!(!succeeds_with_value!(f64::INFINITY));
+    }
+
+    #[test]
+    fn test_read_process_availabilities_from_iter_bad_overlapping_time_slices() {
+        let process_ids = iter::once("process1".into()).collect();
+        let time_slice_info = get_time_slice_info();
+
+        let value = 0.5;
+        let avail = ["winter", "winter.day"].map(|time_slice| ProcessAvailabilityRaw {
+            process_id: "process1".into(),
+            limit_type: "fx".into(),
+            time_slice: time_slice.into(),
+            value,
+        });
+        assert!(read_process_availabilities_from_iter(
+            avail.into_iter(),
+            &process_ids,
+            &time_slice_info
+        )
+        .is_err());
+    }
+
+    #[test]
+    fn test_read_process_availabilities_from_iter_bad_process_id() {
+        assert!(!check_succeeds("MADEUP", "fx", "winter", 0.5));
+    }
+
+    #[test]
+    fn test_read_process_availabilities_from_iter_bad_limit_type() {
+        assert!(!check_succeeds("process1", "MADEUP", "winter", 0.5));
+    }
+
+    #[test]
+    fn test_read_process_availabilities_from_iter_bad_time_slice() {
+        assert!(!check_succeeds("process1", "fx", "MADEUP", 0.5));
+    }
+}
