a5a0d4ba02e1542c7f6177f577226d8b0273f910
Alex Dewar <a.dewar@imperial.ac.uk>
2025-02-10 18:36:59 +0000

    Don't make DEFAULT_LOG_LEVEL depend on cfg(test)
    It doesn't work for integration tests, which is what we need it for.

--- src/log.rs
+++ src/log.rs
@@ -14,9 +14,7 @@ use std::io::IsTerminal;
 use std::path::Path;
 
 /// The default log level for the program.
-///
-/// Note that we disable logging when running tests.
-const DEFAULT_LOG_LEVEL: &str = if cfg!(test) { "off" } else { "info" };
+const DEFAULT_LOG_LEVEL: &str = "info";
 
 /// The file name for the log file containing messages about the ordinary operation of MUSE 2.0
 const LOG_INFO_FILE_NAME: &str = "muse2_info.log";
