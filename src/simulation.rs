eab97eaeee5abf40db0d2f9d8e7d9b1444f9daa2
Alex Dewar <a.dewar@imperial.ac.uk>
2025-02-11 18:59:41 +0000

    Fix: Commodity prices are time-sliced

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,6 +1,7 @@
 //! Functionality for running the MUSE 2.0 simulation.
 use crate::agent::{Asset, AssetPool};
 use crate::model::Model;
+use crate::time_slice::TimeSliceID;
 use log::info;
 use std::collections::HashMap;
 use std::rc::Rc;
@@ -12,8 +13,29 @@ use investment::perform_agent_investment;
 pub mod update;
 use update::{update_commodity_flows, update_commodity_prices};
 
-/// A map relating commodity ID to current price (endogenous)
-pub type CommodityPrices = HashMap<Rc<str>, f64>;
+/// A combination of commodity ID and time slice
+type CommodityPriceKey = (Rc<str>, TimeSliceID);
+
+/// A map relating commodity ID + time slice to current price (endogenous)
+#[derive(Default)]
+pub struct CommodityPrices(HashMap<CommodityPriceKey, f64>);
+
+impl CommodityPrices {
+    /// Get the price for the given commodity and time slice
+    pub fn get(&self, commodity_id: &Rc<str>, time_slice: &TimeSliceID) -> f64 {
+        let key = (Rc::clone(commodity_id), time_slice.clone());
+        *self
+            .0
+            .get(&key)
+            .expect("Missing price for given commodity and time slice")
+    }
+
+    /// Insert a price for the given commodity and time slice
+    pub fn insert(&mut self, commodity_id: &Rc<str>, time_slice: &TimeSliceID, price: f64) {
+        let key = (Rc::clone(commodity_id), time_slice.clone());
+        self.0.insert(key, price);
+    }
+}
 
 /// Run the simulation.
 ///
@@ -22,8 +44,7 @@ pub type CommodityPrices = HashMap<Rc<str>, f64>;
 /// * `model` - The model to run
 /// * `assets` - The asset pool
 pub fn run(model: Model, mut assets: AssetPool) {
-    // Commodity prices (endogenous)
-    let mut prices = CommodityPrices::new();
+    let mut prices = CommodityPrices::default();
 
     for year in model.iter_years() {
         info!("Milestone year: {year}");
