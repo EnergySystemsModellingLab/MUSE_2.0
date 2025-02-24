40c9a8dd0cdbf754420a4207fd2aa34cc8430418
Alex Dewar <a.dewar@imperial.ac.uk>
2025-02-24 18:32:56 +0000

    factor utilisation map code out

--- src/simulation/investment.rs
+++ src/simulation/investment.rs
@@ -18,6 +18,25 @@ where
 {
     info!("Performing agent investment...");
 
+    let utilisation = create_utilisation_map(flows, assets);
+    for (agent_id, vec) in utilisation {
+        for (asset_id, value) in vec {
+            let asset = assets.get(asset_id);
+            info!(
+                "Agent {}, process {}: utilisation {}",
+                agent_id, asset.process.id, value
+            );
+        }
+    }
+}
+
+fn create_utilisation_map<'a, I>(
+    flows: I,
+    assets: &mut AssetPool,
+) -> HashMap<Rc<str>, Vec<(AssetID, f64)>>
+where
+    I: Iterator<Item = (AssetID, &'a Rc<str>, &'a TimeSliceID, f64)>,
+{
     let mut utilisation = HashMap::new();
     for (asset_id, commodity_id, _time_slice, flow) in flows {
         let asset = assets.get(asset_id);
@@ -26,17 +45,29 @@ where
             continue;
         }
 
-        let value = utilisation.entry(asset_id).or_insert(0.0);
+        let vec = utilisation
+            .entry(Rc::clone(&asset.agent_id))
+            .or_insert_with(Vec::new);
+        let value = match vec
+            .iter_mut()
+            .find(|(asset_id2, _value)| asset_id == *asset_id2)
+        {
+            Some(value) => &mut value.1,
+            None => {
+                vec.push((asset_id, 0.0));
+                &mut vec.last_mut().unwrap().1
+            }
+        };
+
         *value += flow.abs();
     }
 
-    for (asset_id, value) in utilisation.iter_mut() {
-        let asset = assets.get(*asset_id);
-        *value /= asset.maximum_activity();
-
-        info!(
-            "Agent {}, process {}: utilisation {}",
-            asset.agent_id, asset.process.id, value
-        );
+    for vec in utilisation.values_mut() {
+        for (asset_id, value) in vec.iter_mut() {
+            let asset = assets.get(*asset_id);
+            *value /= asset.maximum_activity();
+        }
     }
+
+    utilisation
 }
