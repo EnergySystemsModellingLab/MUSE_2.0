cc38542335c555164e40799bc7c88abf5cffcfe6
Alex Dewar <a.dewar@imperial.ac.uk>
2025-02-27 10:27:50 +0000

    perform_agent_investment should take solution as argument

--- src/simulation/investment.rs
+++ src/simulation/investment.rs
@@ -1,4 +1,5 @@
 //! Code for performing agent investment.
+use super::optimisation::Solution;
 use crate::agent::AssetPool;
 use crate::model::Model;
 use log::info;
@@ -8,7 +9,8 @@ use log::info;
 /// # Arguments
 ///
 /// * `model` - The model
+/// * `solution` - The solution to the dispatch optimisation
 /// * `assets` - The asset pool
-pub fn perform_agent_investment(_model: &Model, _assets: &mut AssetPool) {
+pub fn perform_agent_investment(_model: &Model, _solution: &Solution, _assets: &mut AssetPool) {
     info!("Performing agent investment...");
 }
