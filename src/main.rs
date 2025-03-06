48f1600434f6757cd64369c5c4d8b0d7807b28b5
Alex Dewar <a.dewar@imperial.ac.uk>
2025-03-06 10:19:32 +0000

    Put CLI command match into own function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use anyhow::Result;
 use clap::Parser;
-use human_panic::{metadata, setup_panic};
-use muse2::commands;
-
 use commands::{
     handle_example_list_command, handle_example_run_command, handle_run_command, Cli, Commands,
     ExampleSubcommands,
 };
+use human_panic::{metadata, setup_panic};
+use muse2::commands;
 
 fn main() {
     setup_panic!(metadata!().support(format!(
@@ -14,13 +14,15 @@ fn main() {
     )));
 
     let cli = Cli::parse();
+    execute_cli_command(cli.command).unwrap_or_else(|err| eprintln!("Error: {:?}", err));
+}
 
-    match cli.command {
+fn execute_cli_command(command: Commands) -> Result<()> {
+    match command {
         Commands::Run { model_dir } => handle_run_command(&model_dir),
         Commands::Example { subcommand } => match subcommand {
             ExampleSubcommands::List => handle_example_list_command(),
             ExampleSubcommands::Run { name } => handle_example_run_command(&name),
         },
     }
-    .unwrap_or_else(|err| eprintln!("{:?}", err))
 }
