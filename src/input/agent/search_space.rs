6d2c3aaea4ef8c13f79b11227ea9ada6c2720254
Tom Bland <tom_bland@hotmail.co.uk>
2025-03-19 17:01:26 +0000

    Add parameter descriptions

--- src/input/agent/search_space.rs
+++ src/input/agent/search_space.rs
@@ -12,9 +12,14 @@ const AGENT_SEARCH_SPACE_FILE_NAME: &str = "agent_search_space.csv";
 
 #[derive(PartialEq, Debug, Deserialize)]
 struct AgentSearchSpaceRaw {
+    /// The agent to apply the search space to.
     pub agent_id: String,
+    /// The commodity to apply the search space to.
     pub commodity_id: String,
+    /// The year to apply the search space to.
     pub year: u32,
+    /// The processes that the agent will consider investing in. Expressed as process IDs separated
+    /// by semicolons or `None`, meaning all processes.
     pub search_space: Option<String>,
 }
 
