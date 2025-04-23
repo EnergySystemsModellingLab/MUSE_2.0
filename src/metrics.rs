3e77939a69d54fbd65332c0d5e5d99e0da40c82e
Tom Bland <tom_bland@hotmail.co.uk>
2025-04-23 13:55:25 +0100

    Add annual_variable_cost calculation

--- src/metrics.rs
+++ src/metrics.rs
@@ -2,6 +2,9 @@
 
 use crate::units::*;
 
+/// Calculates the capital recovery factor (CRF) for a given lifetime and discount rate.
+///
+/// The CRF is used to annualize capital costs over the lifetime of an asset.
 pub fn capital_recovery_factor(lifetime: IYear, discount_rate: Dimensionless) -> Dimensionless {
     if lifetime == IYear(0) {
         return Dimensionless(0.0);
@@ -13,6 +16,7 @@ pub fn capital_recovery_factor(lifetime: IYear, discount_rate: Dimensionless) ->
     (discount_rate * factor) / (factor - Dimensionless(1.0))
 }
 
+/// Calculates the annual capital cost for a technology
 pub fn annual_capital_cost(
     capital_cost: MoneyPerCapacity,
     capacity: Capacity,
@@ -25,6 +29,7 @@ pub fn annual_capital_cost(
     MoneyPerYear(annual_capital_cost.0) // this is an annualized quantity, so we return it as such
 }
 
+/// Calculates the annual fixed operating cost for a technology
 pub fn annual_fixed_operating_cost(
     fixed_operating_cost: MoneyPerCapacityPerYear,
     capacity: Capacity,
@@ -32,6 +37,9 @@ pub fn annual_fixed_operating_cost(
     fixed_operating_cost * capacity
 }
 
+/// Calculates the annual fixed costs for a technology
+///
+/// This is the sum of the annual capital cost and the annual fixed operating cost.
 pub fn annual_fixed_costs(
     capital_cost: MoneyPerCapacity,
     capacity: Capacity,
@@ -42,4 +50,16 @@ pub fn annual_fixed_costs(
     let annual_capital_cost = annual_capital_cost(capital_cost, capacity, lifetime, discount_rate);
     let annual_fixed_operating_cost = annual_fixed_operating_cost(fixed_operating_cost, capacity);
     annual_capital_cost + annual_fixed_operating_cost
+}
+
+/// Calculates the annual variable cost for a technology
+pub fn annual_variable_cost(
+    variable_operating_cost: MoneyPerActivity,
+    capacity: Capacity,
+    cap2act: ActivityPerCapacity,
+    utilization: Dimensionless,
+) -> MoneyPerYear {
+    let capacity_a = capacity * cap2act;
+    let annual_variable_cost = variable_operating_cost * capacity_a * utilization;
+    MoneyPerYear(annual_variable_cost.0) // this is an annualized quantity, so we return it as such
 }
