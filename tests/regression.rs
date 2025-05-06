69e3450f5e029baa68b04afb1afff6adc46636c8
Alex Dewar <a.dewar@imperial.ac.uk>
2025-05-06 14:09:17 +0100

    Split up big test function

--- tests/regression.rs
+++ tests/regression.rs
@@ -9,6 +9,7 @@ use rstest::rstest;
 use std::fs::{read_dir, File};
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
+use std::sync::OnceLock;
 use tempfile::tempdir;
 
 /// Regression tests for the example models.
@@ -17,79 +18,24 @@ use tempfile::tempdir;
 fn test_regression(#[case] example_name: &str) {
     std::env::set_var("MUSE2_LOG_LEVEL", "off");
 
-    let is_float = Regex::new(r"^-?[0-9]+\.[0-9]+$").unwrap();
     let tempdir = tempdir().unwrap();
     let output_dir = tempdir.path();
     handle_example_run_command(example_name, Some(output_dir)).unwrap();
 
-    let file_names = get_csv_file_names(output_dir);
     let test_data_dir = PathBuf::from(format!("tests/data/{example_name}"));
-    let expected_file_names = get_csv_file_names(&test_data_dir);
+    compare_output_dirs(output_dir, &test_data_dir);
+}
+
+fn compare_output_dirs(output_dir1: &Path, output_dir2: &Path) {
+    let file_names1 = get_csv_file_names(output_dir1);
+    let file_names2 = get_csv_file_names(output_dir2);
 
     // Check that output files haven't been added/removed
-    assert!(file_names == expected_file_names);
+    assert!(file_names1 == file_names2);
 
     let mut errors = Vec::new();
-    for file_name in file_names {
-        let lines1 = read_lines(&output_dir.join(&file_name));
-        let lines2 = read_lines(&test_data_dir.join(&file_name));
-
-        // Check for different number of lines
-        if lines1.len() != lines2.len() {
-            errors.push(format!(
-                "{}: Different number of lines: {} vs {}",
-                file_name,
-                lines1.len(),
-                lines2.len()
-            ));
-        }
-
-        // Compare each line
-        for (num, (line1, line2)) in lines1.into_iter().zip(lines2).enumerate() {
-            let fields1 = line1.split(",").collect_vec();
-            let fields2 = line2.split(",").collect_vec();
-            if fields1.len() != fields2.len() {
-                errors.push(format!(
-                    "{}: line {}: Different number of fields: {} vs {}",
-                    file_name,
-                    num,
-                    fields1.len(),
-                    fields2.len()
-                ));
-            }
-
-            // Lambda for approximately comparing floating-point fields
-            let compare_float_fields = |field1: &str, field2: &str| {
-                // Use a regex to filter out non-floating point values, as well as things that are
-                // technically valid but have strange properties (e.g. inf, NaN)
-                if !is_float.is_match(field1) || !is_float.is_match(field2) {
-                    return None;
-                }
-
-                let float1: f64 = field1.parse().unwrap();
-                let float2: f64 = field2.parse().unwrap();
-                Some(approx_eq!(f64, float1, float2))
-            };
-
-            // Compare each field in turn
-            let mut line_matches = true;
-            for (field1, field2) in fields1.into_iter().zip(fields2) {
-                // First try to compare fields as floating-point values, falling back on string
-                // comparison
-                let result =
-                    compare_float_fields(field1, field2).unwrap_or_else(|| field1 == field2);
-                if !result {
-                    line_matches = false;
-                }
-            }
-
-            if !line_matches {
-                errors.push(format!(
-                    "{}: line {}:\n    + \"{}\"\n    - \"{}\"",
-                    file_name, num, line1, line2
-                ))
-            }
-        }
+    for file_name in file_names1 {
+        compare_lines(output_dir1, output_dir2, &file_name, &mut errors);
     }
 
     assert!(
@@ -97,6 +43,79 @@ fn test_regression(#[case] example_name: &str) {
         "The following errors occurred:\n  * {}",
         errors.join("\n  * ")
     );
+}
+
+fn compare_lines(
+    output_dir1: &Path,
+    output_dir2: &Path,
+    file_name: &str,
+    errors: &mut Vec<String>,
+) {
+    let lines1 = read_lines(&output_dir1.join(file_name));
+    let lines2 = read_lines(&output_dir2.join(file_name));
+
+    // Check for different number of lines
+    if lines1.len() != lines2.len() {
+        errors.push(format!(
+            "{}: Different number of lines: {} vs {}",
+            file_name,
+            lines1.len(),
+            lines2.len()
+        ));
+    }
+
+    // Compare each line
+    for (num, (line1, line2)) in lines1.into_iter().zip(lines2).enumerate() {
+        if !compare_line(num, &line1, &line2, file_name, errors) {
+            errors.push(format!(
+                "{}: line {}:\n    + \"{}\"\n    - \"{}\"",
+                file_name, num, line1, line2
+            ))
+        }
+    }
+}
+
+fn compare_line(
+    num: usize,
+    line1: &str,
+    line2: &str,
+    file_name: &str,
+    errors: &mut Vec<String>,
+) -> bool {
+    let fields1 = line1.split(",").collect_vec();
+    let fields2 = line2.split(",").collect_vec();
+    if fields1.len() != fields2.len() {
+        errors.push(format!(
+            "{}: line {}: Different number of fields: {} vs {}",
+            file_name,
+            num,
+            fields1.len(),
+            fields2.len()
+        ));
+    }
+
+    // Check every field matches
+    fields1.into_iter().zip(fields2).all(|(f1, f2)| {
+        // First try to compare fields as floating-point values, falling back on string comparison
+        try_compare_floats(f1, f2).unwrap_or_else(|| f1 == f2)
+    })
+}
+
+fn try_compare_floats(s1: &str, s2: &str) -> Option<bool> {
+    // Use a regex to filter out non-floating point values, as well as things that are
+    // technically valid but have strange properties (e.g. inf, NaN)
+    static IS_FLOAT: OnceLock<Regex> = OnceLock::new();
+    let is_float = IS_FLOAT.get_or_init(|| Regex::new(r"^-?[0-9]+\.[0-9]+$").unwrap());
+
+    if !is_float.is_match(s1) || !is_float.is_match(s2) {
+        return None;
+    }
+
+    // We can safely unwrap because we know they're valid floats
+    let float1: f64 = s1.parse().unwrap();
+    let float2: f64 = s2.parse().unwrap();
+
+    Some(approx_eq!(f64, float1, float2))
 }
 
 /// Get the names of CSV files expected to appear in the given folder
