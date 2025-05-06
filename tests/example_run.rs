6603cdc21ab17ec2c78f425db916460cb262ab50
Alex Dewar <a.dewar@imperial.ac.uk>
2025-05-06 11:08:43 +0100

    Make `test_handle_example_run_command` a parameterised test

--- tests/example_run.rs
+++ tests/example_run.rs
@@ -1,10 +1,14 @@
-/// Integration tests for the `example run` command.
+//! Integration tests for the `example run` command.
+//!
+//! If you add a new example, you must add a test case below.
 use muse2::commands::handle_example_run_command;
+use rstest::rstest;
 use tempfile::tempdir;
 
 /// An integration test for the `example run` command.
-#[test]
-fn test_handle_example_run_command() {
+#[rstest]
+#[case("simple")]
+fn test_handle_example_run_command(#[case] example_name: &str) {
     std::env::set_var("MUSE2_LOG_LEVEL", "off");
-    handle_example_run_command("simple", Some(tempdir().unwrap().path())).unwrap();
+    handle_example_run_command(example_name, Some(tempdir().unwrap().path())).unwrap();
 }
