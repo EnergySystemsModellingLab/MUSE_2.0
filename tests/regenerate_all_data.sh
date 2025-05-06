66bcaf10d0c4730646231b40bb8fee0c575128ed
Alex Dewar <a.dewar@imperial.ac.uk>
2025-05-06 14:57:05 +0100

    Add script for regenerating data

---
+++ tests/regenerate_all_data.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mydir=$(dirname "$0")
+cd "$mydir"
+
+echo Building MUSE 2.0
+examples=$(cargo run example list 2> /dev/null)
+
+for example in $examples; do
+    echo Generating data for example: $example
+    env MUSE2_LOG_LEVEL=off cargo run example run -o "data/$example" "$example" 2> /dev/null
+done
