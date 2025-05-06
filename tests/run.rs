8c99406380b6e94b2931c3672dcc0baa5b7d3ceb
Alex Dewar <a.dewar@imperial.ac.uk>
2025-05-06 12:52:53 +0100

    Tests: Check that directory creation works for `muse2 run`

--- tests/run.rs
+++ tests/run.rs
@@ -12,7 +12,13 @@ fn get_model_dir() -> PathBuf {
 #[test]
 fn test_handle_run_command() {
     std::env::set_var("MUSE2_LOG_LEVEL", "off");
-    handle_run_command(&get_model_dir(), Some(tempdir().unwrap().path())).unwrap();
+
+    {
+        // Save results to non-existent directory to check that directory creation works
+        let tempdir = tempdir().unwrap();
+        let output_dir = tempdir.path().join("results");
+        handle_run_command(&get_model_dir(), Some(&output_dir)).unwrap();
+    }
 
     // Second time will fail because the logging is already initialised
     assert_eq!(
