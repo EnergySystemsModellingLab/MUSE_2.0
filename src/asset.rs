c516cdf6b397471166a350ea4f861bd590ba359c
Alex Dewar <a.dewar@imperial.ac.uk>
2025-05-12 17:37:29 +0100

    Add `AssetPool::iter_for_region_and_agent`

--- src/asset.rs
+++ src/asset.rs
@@ -226,6 +226,16 @@ impl AssetPool {
         self.iter().filter(|asset| asset.region_id == *region_id)
     }
 
+    /// Iterate over active assets for in a given region a particular agent
+    pub fn iter_for_region_and_agent<'a>(
+        &'a self,
+        region_id: &'a RegionID,
+        agent_id: &'a AgentID,
+    ) -> impl Iterator<Item = &'a Asset> {
+        self.iter_for_region(region_id)
+            .filter(|asset| asset.agent_id == *agent_id)
+    }
+
     /// Iterate over only the active assets in a given region that produce or consume a given
     /// commodity
     pub fn iter_for_region_and_commodity<'a>(
