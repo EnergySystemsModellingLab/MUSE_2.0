89e27a8beeb1d5e84199d8887d8e8f67997e2f69
Alex Dewar <a.dewar@imperial.ac.uk>
2025-05-20 15:51:35 +0100

    Document `time_slices.yaml`

--- docs/generate_input_format_doc.py
+++ docs/generate_input_format_doc.py
@@ -12,6 +12,7 @@ _FILE_ORDER = {
     "Agents": ["agents", "agent_*"],
     "Assets": ["assets"],
     "Commodities": ["commodities", "commodity_costs", "demand", "demand_slicing"],
+    "Time slices": ["time_slices"],
 }
 
 
