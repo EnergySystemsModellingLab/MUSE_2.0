783b8cbcc85c1f83b691c21a1c3e7ec78a136af3
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2025-06-18 21:30:08 +0100

    Tidy up units module

--- src/units.rs
+++ src/units.rs
@@ -2,214 +2,122 @@
 
 use serde::{Deserialize, Serialize};
 use std::fmt;
-use std::iter::Sum;
 use std::ops::{AddAssign, SubAssign};
 
-/// Represents a dimensionless quantity.
-#[derive(
-    Debug,
-    Clone,
-    Copy,
-    PartialEq,
-    PartialOrd,
-    Serialize,
-    Deserialize,
-    derive_more::Add,
-    derive_more::Sub,
-)]
-pub struct Dimensionless(pub f64);
+macro_rules! base_unit_struct {
+    ($name:ident) => {
+        /// A unit type representing a dimensionless value.
+        #[derive(
+            Debug,
+            Clone,
+            Copy,
+            PartialEq,
+            PartialOrd,
+            Serialize,
+            Deserialize,
+            derive_more::Add,
+            derive_more::Sub,
+        )]
+        pub struct $name(pub f64);
 
-impl float_cmp::ApproxEq for Dimensionless {
-    type Margin = float_cmp::F64Margin;
-
-    fn approx_eq<T: Into<Self::Margin>>(self, other: Self, margin: T) -> bool {
-        self.0.approx_eq(other.0, margin)
-    }
+        impl std::ops::Mul<$name> for $name {
+            type Output = $name;
+            fn mul(self, rhs: $name) -> $name {
+                $name(self.0 * rhs.0)
+            }
+        }
+        impl std::ops::Div<$name> for $name {
+            type Output = $name;
+            fn div(self, rhs: $name) -> $name {
+                $name(self.0 / rhs.0)
+            }
+        }
+        impl std::iter::Sum for $name {
+            fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+                iter.fold($name(0.0), |a, b| $name(a.0 + b.0))
+            }
+        }
+        impl AddAssign for $name {
+            fn add_assign(&mut self, other: Self) {
+                self.0 += other.0;
+            }
+        }
+        impl SubAssign for $name {
+            fn sub_assign(&mut self, other: Self) {
+                self.0 -= other.0;
+            }
+        }
+        impl fmt::Display for $name {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+        impl float_cmp::ApproxEq for $name {
+            type Margin = float_cmp::F64Margin;
+            fn approx_eq<T: Into<Self::Margin>>(self, other: Self, margin: T) -> bool {
+                self.0.approx_eq(other.0, margin)
+            }
+        }
+        impl $name {
+            /// Returns the underlying f64 value.
+            pub fn value(self) -> f64 {
+                self.0
+            }
+            /// Returns true if the value is a normal number.
+            pub fn is_normal(self) -> bool {
+                self.0.is_normal()
+            }
+        }
+    };
 }
 
-impl std::ops::Mul for Dimensionless {
-    type Output = Dimensionless;
+// Define Dimensionless first
+base_unit_struct!(Dimensionless);
 
-    fn mul(self, rhs: Dimensionless) -> Self::Output {
-        Dimensionless::from(self.0 * rhs.0)
-    }
-}
-
-impl std::ops::Div for Dimensionless {
-    type Output = Dimensionless;
-
-    fn div(self, rhs: Dimensionless) -> Self::Output {
-        Dimensionless::from(self.0 / rhs.0)
-    }
-}
-
-impl Dimensionless {
-    /// Raise the dimensionless quantity to the power of an integer.
-    pub fn powi(self, rhs: i32) -> Self {
-        Dimensionless::from(self.0.powi(rhs))
-    }
-    /// Returns the absolute value of the dimensionless quantity.
-    pub fn abs(self) -> Self {
-        Dimensionless::from(self.0.abs())
-    }
-    /// Returns true if the value is a normal floating point number.
-    pub fn is_normal(self) -> bool {
-        self.0.is_normal()
-    }
-    /// Returns true if the absolute difference between self and other is less than or equal to epsilon.
-    pub fn approx_eq(self, other: Self, epsilon: f64) -> bool {
-        (self.0 - other.0).abs() <= epsilon
-    }
-}
-
+// Add extra methods for Dimensionless
 impl From<f64> for Dimensionless {
     fn from(val: f64) -> Self {
         Self(val)
     }
 }
-
 impl From<Dimensionless> for f64 {
     fn from(val: Dimensionless) -> Self {
         val.0
     }
 }
 
-impl Sum for Dimensionless {
-    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(Dimensionless(0.0), |a, b| Dimensionless(a.0 + b.0))
+impl Dimensionless {
+    /// Returns the absolute value of this dimensionless number.
+    pub fn abs(self) -> Self {
+        Dimensionless(self.0.abs())
+    }
+    /// Raises this dimensionless number to the power of `rhs`.
+    pub fn powi(self, rhs: i32) -> Self {
+        Dimensionless(self.0.powi(rhs))
     }
 }
 
-impl AddAssign for Dimensionless {
-    fn add_assign(&mut self, other: Self) {
-        self.0 += other.0;
-    }
-}
-
-impl SubAssign for Dimensionless {
-    fn sub_assign(&mut self, other: Self) {
-        self.0 -= other.0;
-    }
-}
-
-impl fmt::Display for Dimensionless {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
+// Define all other units with Dimensionless interactions
 macro_rules! unit_struct {
     ($name:ident) => {
-        /// Represents a type of quantity.
-        #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, derive_more::Add, derive_more::Sub)]
-        pub struct $name(pub f64);
-
-        impl float_cmp::ApproxEq for $name {
-            type Margin = float_cmp::F64Margin;
-
-            fn approx_eq<T: Into<Self::Margin>>(self, other: Self, margin: T) -> bool {
-                self.0.approx_eq(other.0, margin)
-            }
-        }
-
-        impl $name {
-            /// Creates a new instance of the unit type from a f64 value.
-            pub fn from(val: f64) -> Self {
-                Self(val)
-            }
-
-            /// Returns the value of the unit type as a f64.
-            pub fn value(self) -> f64 {
-                self.0
-            }
-
-            /// Returns true if the value is a normal floating point number.
-            pub fn is_normal(self) -> bool {
-                self.0.is_normal()
-            }
-        }
+        base_unit_struct!($name);
 
         impl std::ops::Mul<Dimensionless> for $name {
             type Output = $name;
             fn mul(self, rhs: Dimensionless) -> $name {
-                $name::from(self.0 * rhs.0)
+                $name(self.0 * rhs.0)
             }
         }
-
         impl std::ops::Mul<$name> for Dimensionless {
             type Output = $name;
             fn mul(self, rhs: $name) -> $name {
-                $name::from(self.0 * rhs.0)
+                $name(self.0 * rhs.0)
             }
         }
-
         impl std::ops::Div<Dimensionless> for $name {
             type Output = $name;
             fn div(self, rhs: Dimensionless) -> $name {
-                $name::from(self.0 / rhs.0)
-            }
-        }
-
-        impl std::iter::Sum for $name {
-            fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-                iter.fold($name(0.0), |a, b| $name(a.0 + b.0))
-            }
-        }
-
-        impl AddAssign for $name {
-            fn add_assign(&mut self, other: Self) {
-                self.0 += other.0;
-            }
-        }
-
-        impl SubAssign for $name {
-            fn sub_assign(&mut self, other: Self) {
-                self.0 -= other.0;
-            }
-        }
-
-        impl std::fmt::Display for $name {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}", self.0)
-            }
-        }
-    };
-}
-
-macro_rules! impl_div {
-    ($Lhs:ty, $Rhs:ty, $Out:ty) => {
-        impl std::ops::Div<$Rhs> for $Lhs {
-            type Output = $Out;
-            fn div(self, rhs: $Rhs) -> $Out {
-                <$Out>::from(self.0 / rhs.0)
-            }
-        }
-
-        impl std::ops::Mul<$Rhs> for $Out {
-            type Output = $Lhs;
-            fn mul(self, by: $Rhs) -> $Lhs {
-                <$Lhs>::from(self.0 * by.0)
-            }
-        }
-
-        impl std::ops::Mul<$Lhs> for $Out {
-            type Output = $Rhs;
-            fn mul(self, by: $Lhs) -> $Rhs {
-                <$Rhs>::from(self.0 * by.0)
-            }
-        }
-
-        impl std::ops::Mul<$Out> for $Rhs {
-            type Output = $Lhs;
-            fn mul(self, by: $Out) -> $Lhs {
-                <$Lhs>::from(self.0 * by.0)
-            }
-        }
-
-        impl std::ops::Mul<$Out> for $Lhs {
-            type Output = $Rhs;
-            fn mul(self, by: $Out) -> $Rhs {
-                <$Rhs>::from(self.0 * by.0)
+                $name(self.0 / rhs.0)
             }
         }
     };
@@ -221,8 +129,6 @@ unit_struct!(Energy);
 unit_struct!(Activity);
 unit_struct!(Capacity);
 unit_struct!(Year);
-
-// Derived quantities
 unit_struct!(EnergyPerYear);
 unit_struct!(MoneyPerYear);
 unit_struct!(MoneyPerEnergy);
@@ -234,15 +140,48 @@ unit_struct!(MoneyPerActivity);
 unit_struct!(MoneyPerActivityPerYear);
 unit_struct!(ActivityPerCapacity);
 
-// Simple relationships
+macro_rules! impl_div {
+    ($Lhs:ident, $Rhs:ident, $Out:ident) => {
+        impl std::ops::Div<$Rhs> for $Lhs {
+            type Output = $Out;
+            fn div(self, rhs: $Rhs) -> $Out {
+                $Out(self.0 / rhs.0)
+            }
+        }
+        impl std::ops::Mul<$Rhs> for $Out {
+            type Output = $Lhs;
+            fn mul(self, by: $Rhs) -> $Lhs {
+                $Lhs(self.0 * by.0)
+            }
+        }
+        impl std::ops::Mul<$Lhs> for $Out {
+            type Output = $Rhs;
+            fn mul(self, by: $Lhs) -> $Rhs {
+                $Rhs(self.0 * by.0)
+            }
+        }
+        impl std::ops::Mul<$Out> for $Rhs {
+            type Output = $Lhs;
+            fn mul(self, by: $Out) -> $Lhs {
+                $Lhs(self.0 * by.0)
+            }
+        }
+        impl std::ops::Mul<$Out> for $Lhs {
+            type Output = $Rhs;
+            fn mul(self, by: $Out) -> $Rhs {
+                $Rhs(self.0 * by.0)
+            }
+        }
+    };
+}
+
+// Derived quantities
 impl_div!(Energy, Year, EnergyPerYear);
 impl_div!(Money, Year, MoneyPerYear);
 impl_div!(Money, Energy, MoneyPerEnergy);
 impl_div!(Money, Capacity, MoneyPerCapacity);
 impl_div!(Money, Activity, MoneyPerActivity);
 impl_div!(Activity, Capacity, ActivityPerCapacity);
-
-// Complex relationships
 impl_div!(EnergyPerYear, Capacity, EnergyPerCapacityPerYear);
 impl_div!(MoneyPerYear, Capacity, MoneyPerCapacityPerYear);
 impl_div!(Money, EnergyPerYear, MoneyPerEnergyPerYear);
