2543ee04c8c7a9273bcb78ef8ea79478a7df931f
Alex Dewar <a.dewar@imperial.ac.uk>
2025-06-18 15:14:00 +0100

    Re-enable regression test
    Closes #594.

--- tests/regression_simple.rs
+++ tests/regression_simple.rs
@@ -2,11 +2,6 @@
 mod regression;
 use regression::run_regression_test;
 
-/// Don't run regression test while we're reworking the optimisation model.
-///
-/// Should be re-enabled when we're finished:
-///  https://github.com/EnergySystemsModellingLab/MUSE_2.0/issues/594
-#[ignore]
 #[test]
 fn test_regression_simple() {
     run_regression_test("simple")
