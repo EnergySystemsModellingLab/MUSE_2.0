ea7e7959d8a976b76a0c06a14e0878a390766060
Alex Dewar <a.dewar@imperial.ac.uk>
2025-06-25 10:01:36 +0100

    Calculate demand for SVD commodities
    Assume it's distributed uniformly between time slices for commodities with a seasonal/annual time slice level.
    
    Closes #645.

--- src/simulation/investment.rs
+++ src/simulation/investment.rs
@@ -2,8 +2,13 @@
 use super::optimisation::FlowMap;
 use super::CommodityPrices;
 use crate::asset::AssetPool;
+use crate::commodity::{Commodity, CommodityType};
 use crate::model::Model;
+use crate::region::RegionID;
+use crate::time_slice::{TimeSliceID, TimeSliceLevel};
+use crate::units::Flow;
 use log::info;
+use std::collections::HashMap;
 
 /// Perform agent investment to determine capacity investment of new assets for next milestone year.
 ///
@@ -13,13 +18,25 @@ use log::info;
 /// * `flow_map` - Map of commodity flows
 /// * `prices` - Commodity prices
 /// * `assets` - The asset pool
+/// * `year` - The current milestone year
 pub fn perform_agent_investment(
-    _model: &Model,
+    model: &Model,
     _flow_map: &FlowMap,
     _prices: &CommodityPrices,
     assets: &mut AssetPool,
+    year: u32,
 ) {
     info!("Performing agent investment...");
+
+    for commodity in model.commodities.values() {
+        if commodity.kind != CommodityType::ServiceDemand {
+            // We look at SVD commodities first
+            continue;
+        }
+
+        // Calculate demand per time slice
+        let _demand = calculate_svd_demand(model, commodity, year);
+    }
 
     let mut new_pool = Vec::new();
     for asset in assets.iter() {
@@ -28,4 +45,49 @@ pub fn perform_agent_investment(
     }
 
     assets.replace_active_pool(new_pool);
+}
+
+/// Get demand per time slice for the given commodity.
+///
+/// For commodities with a time slice level of "daynight", this information is provided directly in
+/// `demand_slices.csv`. If the time slice level is seasonal or annual, we assume the user-specified
+/// demand is uniformly distributed for either each season or the whole year.
+pub fn calculate_svd_demand(
+    model: &Model,
+    commodity: &Commodity,
+    year: u32,
+) -> HashMap<(RegionID, TimeSliceID), Flow> {
+    // Sanity check
+    assert!(commodity.kind == CommodityType::ServiceDemand);
+
+    let mut map = HashMap::new();
+    for region_id in model.iter_regions() {
+        for ts_selection in model
+            .time_slice_info
+            .iter_selections_at_level(commodity.time_slice_level)
+        {
+            let demand_for_selection = *commodity
+                .demand
+                .get(&(region_id.clone(), year, ts_selection.clone()))
+                .unwrap();
+
+            // Assume demand is uniformly distributed between time slices in `ts_selection`
+            let demand_iter = model
+                .time_slice_info
+                .calculate_share(
+                    &ts_selection,
+                    TimeSliceLevel::DayNight,
+                    demand_for_selection,
+                )
+                .unwrap();
+
+            for (ts_selection, demand) in demand_iter {
+                // Safe: we know this is a time slice
+                let time_slice = ts_selection.try_into().unwrap();
+                map.insert((region_id.clone(), time_slice), demand);
+            }
+        }
+    }
+
+    map
 }
