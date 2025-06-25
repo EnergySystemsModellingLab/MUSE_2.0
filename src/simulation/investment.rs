52bd989ad106f3789e130390d6c9944c87fd7343
Alex Dewar <a.dewar@imperial.ac.uk>
2025-06-25 10:04:20 +0100

    perform_agent_investment: Comment out code for selecting assets for now

--- src/simulation/investment.rs
+++ src/simulation/investment.rs
@@ -17,15 +17,10 @@ pub fn perform_agent_investment(
     _model: &Model,
     _flow_map: &FlowMap,
     _prices: &CommodityPrices,
-    assets: &mut AssetPool,
+    _assets: &mut AssetPool,
 ) {
     info!("Performing agent investment...");
 
-    let mut new_pool = Vec::new();
-    for asset in assets.iter() {
-        // **TODO**: Implement agent investment. For now, just keep all assets.
-        new_pool.push(asset.clone().into());
-    }
-
-    assets.replace_active_pool(new_pool);
+    // **TODO:** Perform agent investment. For now, let's just leave the pool unmodified.
+    // assets.replace_active_pool(new_pool);
 }
