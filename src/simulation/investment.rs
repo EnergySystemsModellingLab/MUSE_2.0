b1d06e19ae757451a1e087409f8110063c2a315a
Alex Dewar <a.dewar@imperial.ac.uk>
2025-06-26 15:07:38 +0100

    wip: do something with tranches

--- src/simulation/investment.rs
+++ src/simulation/investment.rs
@@ -34,7 +34,13 @@ pub fn perform_agent_investment(
     let (load, peak_load) = calculate_load(&model.time_slice_info, &demand);
 
     for tranche_num in 0..model.num_demand_tranches {
-        do_something_with_demand_tranches(model, &load, &peak_load, tranche_num);
+        do_something_with_demand_tranches(
+            &model.time_slice_info,
+            &load,
+            &peak_load,
+            tranche_num,
+            model.num_demand_tranches,
+        );
     }
 
     // **TODO:** Perform agent investment. For now, let's just leave the pool unmodified.
@@ -84,32 +90,86 @@ fn calculate_load(time_slice_info: &TimeSliceInfo, demand: &DemandMap) -> (LoadM
 }
 
 fn do_something_with_demand_tranches(
-    model: &Model,
+    time_slice_info: &TimeSliceInfo,
     load: &LoadMap,
     peak_load: &PeakLoadMap,
     tranche_num: u32,
+    num_tranches: u32,
 ) {
-    let num_tranches = model.num_demand_tranches;
     for ((commodity_id, region_id), &peak_load) in peak_load.iter() {
         let tranche_width = peak_load / Dimensionless(num_tranches as f64);
-        let tranche_bottom = Dimensionless((num_tranches - tranche_num - 1) as f64) * tranche_width;
+        // let tranche_bottom = (Dimensionless(tranche_num as f64)) * tranche_width;
+        let tranche_top = Dimensionless((tranche_num + 1) as f64) * tranche_width;
+        // let tranche_top = Flow(4.0);
 
         let mut sum_load = Flow(0.0);
-        let mut count = 0;
-        for time_slice in model.time_slice_info.iter_ids() {
-            let load = *load
+        for time_slice in time_slice_info.iter_ids() {
+            let load = load
                 .get(&(commodity_id.clone(), region_id.clone(), time_slice.clone()))
-                .unwrap();
-            if load >= tranche_bottom {
-                sum_load += load;
-                count += 1;
-            }
+                .unwrap()
+                .min(tranche_top);
+            sum_load += load;
         }
-        let mean_load = sum_load / Dimensionless(count as f64);
-        let load_factor = mean_load / peak_load;
+        let mean_load = sum_load / Dimensionless(time_slice_info.time_slices.len() as f64);
+        let load_factor = mean_load / tranche_top;
+
         info!(
             "Tranche {}: LF for {}: {}",
             tranche_num, commodity_id, load_factor
         );
+
+        // return load_factor;
+    }
+
+    // unreachable!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::fixture::{commodity_id, region_id};
+    use crate::time_slice::{Season, TimeOfDay, TimeSliceInfo};
+    // use float_cmp::assert_approx_eq;
+    use indexmap::IndexSet;
+    use rstest::rstest;
+    use std::iter;
+
+    #[rstest]
+    fn test_tranches(commodity_id: CommodityID, region_id: RegionID) {
+        let season: Season = "season1".into();
+        let times_of_day: IndexSet<TimeOfDay> = (1..=3).map(|i| format!("ts{i}").into()).collect();
+        let time_slices = times_of_day
+            .iter()
+            .zip([0.2, 0.3, 0.5])
+            .map(|(tod, dur): (_, f64)| {
+                (
+                    TimeSliceID {
+                        season: season.clone(),
+                        time_of_day: tod.clone(),
+                    },
+                    Dimensionless(dur),
+                )
+            })
+            .collect();
+        let time_slice_info = TimeSliceInfo {
+            times_of_day,
+            seasons: iter::once((season, Dimensionless(1.0))).collect(),
+            time_slices,
+        };
+
+        let demand = time_slice_info
+            .iter_ids()
+            .map(|time_slice| {
+                (
+                    (commodity_id.clone(), region_id.clone(), time_slice.clone()),
+                    Flow(2.0),
+                )
+            })
+            .collect();
+        let (load, peak_load) = calculate_load(&time_slice_info, &demand);
+        let _load_factor =
+            do_something_with_demand_tranches(&time_slice_info, &load, &peak_load, 0, 2);
+
+        // assert_approx_eq!(Dimensionless, load_factor, Dimensionless(1.0));
     }
 }
