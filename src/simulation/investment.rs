001144c8daaa08b46fa4f773d92912a4fd98c419
Alex Dewar <a.dewar@imperial.ac.uk>
2025-06-26 17:47:28 +0100

    wip

--- src/simulation/investment.rs
+++ src/simulation/investment.rs
@@ -7,6 +7,7 @@ use crate::model::Model;
 use crate::region::RegionID;
 use crate::time_slice::{TimeSliceID, TimeSliceInfo};
 use crate::units::{Dimensionless, Flow};
+use indexmap::IndexMap;
 use itertools::Itertools;
 use log::info;
 use std::collections::HashMap;
@@ -39,10 +40,17 @@ pub fn perform_agent_investment(
         }
 
         for region_id in model.iter_regions() {
-            let (_load_map, peak) =
+            let (load_map, peak) =
                 calculate_load(&model.time_slice_info, commodity_id, region_id, &demand);
             let tranches = get_tranches(peak, model.num_demand_tranches);
             info!("{}: {:?}", commodity_id, tranches);
+
+            for (i, tranche) in tranches.iter().enumerate() {
+                let tranche_load = calculate_load_in_tranche(&load_map, tranche);
+                info!("{:?}", tranche_load.values());
+                let load_factor = calculate_load_factor(&model.time_slice_info, &tranche_load);
+                info!("Tranche {i}: LF: {load_factor}");
+            }
         }
     }
 
@@ -71,13 +79,14 @@ pub fn calculate_svd_demand_profile(commodities: &CommodityMap, flow_map: &FlowM
     map
 }
 
+/// NB: USING INDEXMAP FOR EASE OF DEBUGGING
 fn calculate_load(
     time_slice_info: &TimeSliceInfo,
     commodity_id: &CommodityID,
     region_id: &RegionID,
     demand: &DemandMap,
-) -> (HashMap<TimeSliceID, Flow>, Flow) {
-    let mut load = HashMap::new();
+) -> (IndexMap<TimeSliceID, Flow>, Flow) {
+    let mut load = IndexMap::new();
     let mut peak_load = Flow(0.0);
 
     for (time_slice, ts_length) in time_slice_info.iter() {
@@ -109,4 +118,42 @@ fn get_tranches(peak: Flow, num_tranches: u32) -> Vec<Range<Flow>> {
     tranches.push(tranche_bottom(num_tranches - 1)..Flow(f64::INFINITY));
 
     tranches
+}
+
+/// NB: USING INDEXMAP FOR EASE OF DEBUGGING
+fn calculate_load_in_tranche(
+    load: &IndexMap<TimeSliceID, Flow>,
+    tranche: &Range<Flow>,
+) -> IndexMap<TimeSliceID, Flow> {
+    load.iter()
+        .map(|(time_slice, &power)| {
+            // SHOULD BOUNDS BE RANGEINCLUSIVE?!
+            let load_in_tranche = if power >= tranche.start {
+                power.min(tranche.end) - tranche.start
+            } else {
+                Flow(0.0)
+            };
+
+            (time_slice.clone(), load_in_tranche)
+        })
+        .collect()
+}
+
+fn calculate_load_factor(
+    time_slice_info: &TimeSliceInfo,
+    load: &IndexMap<TimeSliceID, Flow>,
+) -> Dimensionless {
+    time_slice_info
+        .time_slices
+        .iter()
+        .map(|(ts, &ts_length)| {
+            if *load.get(ts).unwrap() > Flow(0.0) {
+                ts_length
+            } else {
+                Dimensionless(0.0)
+            }
+        })
+        .sum()
+
+    // TODO: CLAMP RETURN VALUE???
 }
