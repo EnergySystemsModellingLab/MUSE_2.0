e57d2730e7693dc578d97a53b839397d617e2b1d
Tom Bland <tom_bland@hotmail.co.uk>
2025-07-08 16:18:35 +0100

    Add calculation for annual capital cost

--- src/simulation/lcox.rs
+++ src/simulation/lcox.rs
@@ -3,8 +3,10 @@
 //! This is more commonly referred to as Levelised Cost of *Electricity*, but as the model can
 //! include other flows, we use the term LCOX.
 use crate::asset::AssetRef;
+use crate::process::Process;
+use crate::region::RegionID;
 use crate::time_slice::TimeSliceID;
-use crate::units::{Capacity, Flow, MoneyPerActivity};
+use crate::units::{Capacity, Dimensionless, Flow, Money, MoneyPerActivity, MoneyPerCapacity};
 use std::collections::HashMap;
 
 /// Calculate LCOX based on the specified reduced costs and demand for a particular tranche.
@@ -28,4 +30,44 @@ pub fn calculate_lcox(
 
     let new_capacity = asset.is_commissioned().then_some(Capacity(43.0));
     (MoneyPerActivity(42.0), new_capacity, unmet)
+}
+
+/// Calculates the capital recovery factor (CRF) for a given lifetime and discount rate.
+///
+/// The CRF is used to annualize capital costs over the lifetime of an asset.
+pub fn capital_recovery_factor(lifetime: u32, discount_rate: Dimensionless) -> Dimensionless {
+    if lifetime == 0 {
+        return Dimensionless(0.0);
+    }
+    if discount_rate == Dimensionless(0.0) {
+        return Dimensionless(1.0) / Dimensionless(lifetime as f64);
+    }
+    let factor = (Dimensionless(1.0) + discount_rate).powi(lifetime as i32);
+    (discount_rate * factor) / (factor - Dimensionless(1.0))
+}
+
+/// Calculates the annual capital cost for a technology
+pub fn annual_capital_cost(
+    capital_cost: MoneyPerCapacity,
+    capacity: Capacity,
+    lifetime: u32,
+    discount_rate: Dimensionless,
+) -> Money {
+    let crf = capital_recovery_factor(lifetime, discount_rate);
+    let total_capital_cost = capital_cost * capacity * crf;
+    total_capital_cost * crf
+}
+
+/// Calculates the annual capital cost for a process.
+pub fn annual_capital_cost_for_process(
+    process: &Process,
+    capacity: Capacity,
+    region: RegionID,
+    year: u32,
+) -> Money {
+    let process_parameter = process.parameters.get(&(region, year)).unwrap();
+    let capital_cost = process_parameter.capital_cost;
+    let lifetime = process_parameter.lifetime;
+    let discount_rate = process_parameter.discount_rate;
+    annual_capital_cost(capital_cost, capacity, lifetime, discount_rate)
 }
