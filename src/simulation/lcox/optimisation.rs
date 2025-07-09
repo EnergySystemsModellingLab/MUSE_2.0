33e3996068e5d6447a375e40474db1baefcb488b
Tom Bland <tom_bland@hotmail.co.uk>
2025-07-09 15:06:52 +0100

    Add activity constraints

--- src/simulation/lcox/optimisation.rs
+++ src/simulation/lcox/optimisation.rs
@@ -182,6 +182,7 @@ pub fn perform_lcox_optimisation(
     );
 
     // Add constraints
+    add_constraints(&mut problem, &variables);
 
     // Solve problem
     let highs_model = problem.optimise(Sense::Minimise);
@@ -195,4 +196,69 @@ pub fn perform_lcox_optimisation(
         }),
         status => Err(anyhow!("Could not solve: {status:?}")),
     }
+}
+
+fn add_constraints(problem: &mut Problem, variables: &VariableMap) {
+    add_activity_constraints_for_assets(problem, &variables.asset_activity_vars);
+    add_activity_constraints_for_candidates(
+        problem,
+        &variables.candidate_capacity_vars,
+        &variables.candidate_activity_vars,
+    );
+    add_capacity_constraints_for_candidates(problem, &variables.candidate_capacity_vars);
+    add_demand_constraints(
+        problem,
+        &variables.asset_activity_vars,
+        &variables.candidate_activity_vars,
+    );
+}
+
+/// NOTE: Copied from `add_activity_constraints` in `optimisation/constraints.rs`.
+fn add_activity_constraints_for_assets(
+    problem: &mut Problem,
+    asset_activity_vars: &IndexMap<(AssetRef, TimeSliceID), Variable>,
+) {
+    for ((asset, time_slice), var) in asset_activity_vars.iter() {
+        let limits = asset.get_activity_limits(time_slice);
+        let limits = limits.start().value()..=limits.end().value();
+        problem.add_row(limits, [(*var, 1.0)]);
+    }
+}
+
+fn add_activity_constraints_for_candidates(
+    problem: &mut Problem,
+    candidate_capacity_vars: &IndexMap<AssetRef, Variable>,
+    candidate_activity_vars: &IndexMap<(AssetRef, TimeSliceID), Variable>,
+) {
+    for ((asset, time_slice), activity_var) in candidate_activity_vars.iter() {
+        let capacity_var = candidate_capacity_vars.get(asset).unwrap();
+        let limits = asset.get_activity_per_capacity_limits(time_slice);
+        let lower_limit = limits.start().value();
+        let upper_limit = limits.end().value();
+
+        // Upper bound: activity ≤ capacity * upper_limit
+        problem.add_row(
+            ..=0.0,
+            [(*activity_var, 1.0), (*capacity_var, -upper_limit)],
+        );
+
+        // Lower bound: activity ≥ capacity * lower_limit
+        problem.add_row(
+            ..=0.0,
+            [(*activity_var, -1.0), (*capacity_var, lower_limit)],
+        );
+    }
+}
+
+fn add_demand_constraints(
+    problem: &mut Problem,
+    asset_activity_vars: &IndexMap<(AssetRef, TimeSliceID), Variable>,
+    candidate_activity_vars: &IndexMap<(AssetRef, TimeSliceID), Variable>,
+) {
+}
+
+fn add_capacity_constraints_for_candidates(
+    problem: &mut Problem,
+    candidate_capacity_vars: &IndexMap<AssetRef, Variable>,
+) {
 }
