9456cf74c58715fb3dc6a95633a788968f94db90
Alex Dewar <a.dewar@imperial.ac.uk>
2025-07-10 15:26:33 +0100

    Asset: Add method to get primary output

--- src/asset.rs
+++ src/asset.rs
@@ -140,6 +140,12 @@ impl Asset {
     pub fn iter_flows(&self) -> impl Iterator<Item = &ProcessFlow> {
         self.get_flows_map().values()
     }
+
+    /// Get the primary output (if any) for this asset
+    pub fn primary_output(&self) -> Option<&ProcessFlow> {
+        let flow = self.iter_flows().next().unwrap();
+        flow.is_primary_output.then_some(flow)
+    }
 }
 
 /// Whether the specified value is a valid capacity for an asset
