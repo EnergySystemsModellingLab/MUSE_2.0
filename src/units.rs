260b79617a79ca0d5f1421cfdbeb74e224841eeb
Alex Dewar <a.dewar@imperial.ac.uk>
2025-07-10 15:59:21 +0100

    Add `total_cmp` method to unit types

--- src/units.rs
+++ src/units.rs
@@ -37,6 +37,8 @@ pub trait UnitType:
     fn max(&self, other: Self) -> Self;
     /// Returns the min of two values
     fn min(&self, other: Self) -> Self;
+    /// Returns ordering between self and other
+    fn total_cmp(&self, other: Self) -> std::cmp::Ordering;
 }
 
 macro_rules! base_unit_struct {
@@ -116,6 +118,10 @@ macro_rules! base_unit_struct {
             pub fn min(&self, other: Self) -> Self {
                 Self(self.0.min(other.0))
             }
+            /// Returns ordering between self and other
+            pub fn total_cmp(&self, other: &Self) -> std::cmp::Ordering {
+                self.0.total_cmp(&other.0)
+            }
         }
         impl UnitType for $name {
             /// Create from an f64 value
@@ -145,6 +151,10 @@ macro_rules! base_unit_struct {
             /// Returns the min of two values
             fn min(&self, other: Self) -> Self {
                 self.min(other)
+            }
+            /// Returns ordering between self and other
+            fn total_cmp(&self, other: Self) -> std::cmp::Ordering {
+                self.total_cmp(&other)
             }
         }
     };
