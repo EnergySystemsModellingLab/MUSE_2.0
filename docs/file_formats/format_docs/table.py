faa1d2111788914560ec6d2f45c02863cab86d6c
Alex Dewar <a.dewar@imperial.ac.uk>
2025-07-22 09:59:58 +0100

    fields2table: Allow for specifying default values

--- docs/file_formats/format_docs/table.py
+++ docs/file_formats/format_docs/table.py
@@ -1,12 +1,23 @@
 from table2md import MarkdownTable
+from yaml import dump
+
+
+def to_yaml_str(value) -> str:
+    return dump(value).removesuffix("\n...\n")
 
 
 def fields2table(fields: list[dict[str, str]]) -> str:
     data = []
     for f in fields:
-        # MarkdownTable can't handle newlines, so replace with HTML equivalent
         notes = f.get("notes", "")
+
+        default = f.get("default", None)
+        if default is not None:
+            notes = f"Optional. Defaults to `{to_yaml_str(default)}`.\n\n{notes}"
+
+        # MarkdownTable can't handle newlines, so replace with HTML equivalent
         notes = notes.replace("\n\n", "<br /><br />").replace("\n", " ")
+
         row = {
             "Field": f"`{f['name']}`",
             "Description": f["description"],
