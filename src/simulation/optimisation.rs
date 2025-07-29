0cb749fa1c45fe39d1fc1acafc6e572e718f8647
Alex Dewar <a.dewar@imperial.ac.uk>
2025-07-29 16:29:05 +0100

    Allow for running dispatch on subset of commodities

--- src/simulation/optimisation.rs
+++ src/simulation/optimisation.rs
@@ -161,6 +161,9 @@ impl Solution<'_> {
 
 /// Perform the dispatch optimisation.
 ///
+/// If `commodities` is provided, the commodity balance constraints will only be added for these
+/// commodities, else they will be added for all commodities.
+///
 /// For a detailed description, please see the [dispatch optimisation formulation][1].
 ///
 /// [1]: https://energysystemsmodellinglab.github.io/MUSE_2.0/model/dispatch_optimisation.html
@@ -170,6 +173,7 @@ impl Solution<'_> {
 /// * `model` - The model
 /// * `asset_pool` - The asset pool
 /// * `candidate_assets` - Candidate assets for inclusion in active pool
+/// * `commodities` - The subset of commodities to apply constraints to
 /// * `year` - Current milestone year
 /// * `run_number` - Which dispatch run for the current year this is
 /// * `data_writer` - For saving output data
@@ -181,12 +185,18 @@ pub fn perform_dispatch_optimisation<'a>(
     model: &'a Model,
     asset_pool: &AssetPool,
     candidate_assets: &[AssetRef],
+    commodities: Option<&[CommodityID]>,
     year: u32,
     run_number: u32,
     writer: &mut DataWriter,
 ) -> Result<Solution<'a>> {
-    let solution =
-        perform_dispatch_optimisation_no_save(model, asset_pool, candidate_assets, year)?;
+    let solution = perform_dispatch_optimisation_no_save(
+        model,
+        asset_pool,
+        candidate_assets,
+        commodities,
+        year,
+    )?;
 
     writer.write_debug_info(year, run_number, &solution)?;
 
@@ -198,6 +208,7 @@ fn perform_dispatch_optimisation_no_save<'a>(
     model: &'a Model,
     asset_pool: &AssetPool,
     candidate_assets: &[AssetRef],
+    commodities: Option<&[CommodityID]>,
     year: u32,
 ) -> Result<Solution<'a>> {
     // Set up problem
@@ -220,7 +231,19 @@ fn perform_dispatch_optimisation_no_save<'a>(
 
     // Add constraints
     let all_assets = chain(asset_pool.iter(), candidate_assets.iter());
-    let constraint_keys = add_asset_constraints(&mut problem, &variables, model, all_assets, year);
+    let mut all_commodities = Vec::new();
+    let commodities = commodities.unwrap_or_else(|| {
+        all_commodities = model.commodities.keys().cloned().collect();
+        &all_commodities
+    });
+    let constraint_keys = add_asset_constraints(
+        &mut problem,
+        &variables,
+        model,
+        all_assets,
+        commodities,
+        year,
+    );
 
     // Solve model
     let solution = problem
