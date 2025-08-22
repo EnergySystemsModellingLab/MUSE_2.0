eb26481f67c40c763233aae832ad56b041e76607
Alex Dewar <a.dewar@imperial.ac.uk>
2025-08-22 12:27:54 +0100

    Add `parse_year_str_with` helper function

--- src/year.rs
+++ src/year.rs
@@ -3,16 +3,6 @@ use crate::input::is_sorted_and_unique;
 use anyhow::{ensure, Context, Result};
 use itertools::Itertools;
 
-/// Parse a single year from a string and check it is in `valid_years`
-fn parse_and_validate_year(s: &str, valid_years: &[u32]) -> Option<u32> {
-    let year = s.trim().parse::<u32>().ok()?;
-    if valid_years.binary_search(&year).is_ok() {
-        Some(year)
-    } else {
-        None
-    }
-}
-
 /// Parse a string of years separated by semicolons into a vector of u32 years.
 ///
 /// The string can be either "all" (case-insensitive), a single year, or a semicolon-separated list
@@ -20,8 +10,8 @@ fn parse_and_validate_year(s: &str, valid_years: &[u32]) -> Option<u32> {
 ///
 /// # Arguments
 ///
-/// - `s` - Input string to parse
-/// - `valid_years` - The possible years which can be referenced in `s` (must be sorted and unique)
+/// * `s` - Input string to parse
+/// * `valid_years` - The possible years which can be referenced in `s` (must be sorted and unique)
 ///
 /// # Returns
 ///
@@ -31,23 +21,54 @@ fn parse_and_validate_year(s: &str, valid_years: &[u32]) -> Option<u32> {
 ///
 /// If `valid_years` is unsorted or non-unique.
 pub fn parse_year_str(s: &str, valid_years: &[u32]) -> Result<Vec<u32>> {
-    // We depend on this in `parse_and_validate_year`
     assert!(
         is_sorted_and_unique(valid_years),
         "`valid_years` must be sorted and unique"
     );
 
+    // Validate by checking year is in valid_years. If `s` == "all", we copy valid_years.
+    let years = parse_year_str_with(s, |year| valid_years.binary_search(&year).is_ok())?
+        .unwrap_or_else(|| valid_years.to_vec());
+
+    Ok(years)
+}
+
+/// Parse a string of years separated by semicolons into a vector of u32 years with the specified
+/// validation function.
+///
+/// The string can be either "all" (case-insensitive), a single year, or a semicolon-separated list
+/// of years (e.g. "2020;2021;2022" or "2020; 2021; 2022")
+///
+/// # Arguments
+///
+/// * `s` - Input string to parse
+/// * `validate_year` - Function for validating parsed year (should return true if valid)
+///
+/// # Returns
+///
+/// * `Err` if parsing or validation failed
+/// * `Ok(Some(Vec<u32>))` if a valid list of years is provided
+/// * `Ok(None)` if the magic string `all` was provided
+pub fn parse_year_str_with<F>(s: &str, validate_year: F) -> Result<Option<Vec<u32>>>
+where
+    F: Fn(u32) -> bool,
+{
     let s = s.trim();
     ensure!(!s.is_empty(), "No years provided");
 
     if s.eq_ignore_ascii_case("all") {
-        return Ok(Vec::from_iter(valid_years.iter().copied()));
+        return Ok(None);
     }
+
+    let parse_and_validate = |year_str: &str| {
+        let year = year_str.trim().parse::<u32>().ok()?;
+        validate_year(year).then_some(year)
+    };
 
     let years: Vec<_> = s
         .split(";")
-        .map(|y| {
-            parse_and_validate_year(y, valid_years).with_context(|| format!("Invalid year: {y}"))
+        .map(|year_str| {
+            parse_and_validate(year_str).with_context(|| format!("Invalid year: {year_str}"))
         })
         .try_collect()?;
 
@@ -56,7 +77,7 @@ pub fn parse_year_str(s: &str, valid_years: &[u32]) -> Result<Vec<u32>> {
         "Years must be in order and unique"
     );
 
-    Ok(years)
+    Ok(Some(years))
 }
 
 #[cfg(test)]
