d9af8dd012fd6c7e9997e372f568edc5ddcbc844
Alex Dewar <a.dewar@imperial.ac.uk>
2025-09-01 16:52:44 +0100

    script: update to handle index.yaml files

--- schemas/validate.py
+++ schemas/validate.py
@@ -5,6 +5,7 @@ import sys
 from pathlib import Path
 import yaml
 import argparse
+from dataclasses import dataclass
 
 SCHEMA_PATH = Path(__file__).parent / "input" / "assets.yaml"
 
@@ -29,27 +30,52 @@ class Validator:
         return errors
 
 
+@dataclass
+class SchemaEntry:
+    validator: Validator
+    path: Path
+
+
+class SchemaIndex:
+    def __init__(self, index_path: Path) -> None:
+        self.entries: list[SchemaEntry] = []
+
+        basedir = index_path.parent
+        with index_path.open() as f:
+            data = yaml.safe_load(f)
+        for entry in data["schemas"]:
+            validator = Validator(basedir / entry["include"])
+            self.entries.append(SchemaEntry(validator, Path(entry["path"])))
+
+    def validate(self, data_dir: Path) -> dict[Path, list[str]]:
+        errors = {}
+        for entry in self.entries:
+            path = data_dir / entry.path
+            cur_errors = entry.validator.validate(path)
+            if cur_errors:
+                errors[path] = cur_errors
+        return errors
+
+
 def main() -> int:
     ret = 0
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--schema", type=Path, required=True)
-    parser.add_argument("file_paths", type=Path, nargs=argparse.REMAINDER)
+    parser.add_argument("--schema-index", type=Path, required=True)
+    parser.add_argument("data_dirs", type=Path, nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
-    # Load schema
-    v = Validator(args.schema)
-
-    # Process files
-    for file_path in args.file_paths:
-        errors = v.validate(file_path)
+    index = SchemaIndex(args.schema_index)
+    for data_dir in args.data_dirs:
+        errors = index.validate(data_dir)
         if not errors:
-            print(f"✅ {file_path} is valid!")
+            print(f"✅ {data_dir} is valid!")
         else:
             ret = 1
-            print(f"❌ {file_path} has errors:")
-            for error in errors:
-                print(f"   - {error}")
+            for file_path, errors in errors.items():
+                print(f"❌ {file_path} has errors:")
+                for error in errors:
+                    print(f"   - {error}")
 
     return ret
 
