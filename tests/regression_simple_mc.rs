7d9be43a257f4769f040207eec1fc8c50af818ad
Alex Dewar <a.dewar@imperial.ac.uk>
2025-09-04 11:16:25 +0100

    Rename example: `simple_mc` => `missing_commodity`

--- tests/regression_simple_mc.rs
+++
@@ -1,8 +0,0 @@
-//! A regression test for the "simple_mc" example
-mod regression;
-use regression::run_regression_test;
-
-#[test]
-fn test_regression_simple_mc() {
-    run_regression_test("simple_mc")
-}
